f0cfb068ea46879501f83eaa2486385c98049f33
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-20 12:15:07 -0700

    working on server weather done with location

--- server.js
+++ server.js
@@ -10,5 +10,34 @@ console.log('Working?', PORT);
 
 const app = express();
 
+app.use(cors());
+
+app.get('/location', (req, resp) =>{
+    let data = req('./data/location.json');
+    let newData = new location(data[0]);
+
+    response.status(200).json(newData);
+
+});
+
+function location(info){
+    this.latitude = info.lat;
+    this.logitude = info.lon;
+    this.formatting_query = info.display_name
+    
+}
+
+app.get('/weather', (req, resp) => {
+    let data = require('./data/weather.json');
+    
+});
+
+
+
+
+
+
+
+
 
 
