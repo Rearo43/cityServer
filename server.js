954b05a2fba856671b5471a8fb1ae2f42864a97a
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-20 13:51:52 -0700

    finished lab 06

--- server.js
+++ server.js
@@ -6,32 +6,55 @@ const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT;
 
-console.log('Working?', PORT);
-
 const app = express();
 
 app.use(cors());
 
 app.get('/location', (req, resp) =>{
-    let data = req('./data/location.json');
-    let newData = new location(data[0]);
+    let dataLocation = req('./data/location.json');
+    let newData = new Location(dataLocation[0]);
 
     response.status(200).json(newData);
 
 });
 
-function location(info){
+function Location(info){
     this.latitude = info.lat;
     this.logitude = info.lon;
     this.formatting_query = info.display_name
-    
+    this.search_query = city;
+
 }
 
 app.get('/weather', (req, resp) => {
-    let data = require('./data/weather.json');
-    
+    let dataWeather = require('./data/weather.json');
+    let allWeather = [];
+
+    dataWeather.data.forEach(dayData => {
+        let weather = new Weather(dayData);
+        allWeather.push(weather);
+
+    });
+    response.status(200).json(allWeather);
+
 });
 
+function Weather(info){
+    this.forcast = info.weather.description;
+    this.datetime = info.datetime;
+}
+
+
+app.use('*', (req,resp) => {
+    resp.status(404).send('Could Not Find!');
+  });
+  
+  app.use((error, req, resp, next) => {
+    console.log(error);
+    resp.status(500).send('Broken Server!');
+  });
+  
+  app.listen( PORT, () => console.log('Working?', PORT));
 
 
 
