c4b6f6b4749d6535c118ce8188c7976c63c5bb9e
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-25 01:22:50 -0700

    starting over server project

--- server1.js
+++ server1.js
@@ -26,6 +26,25 @@ function Location(info, city){
 
 }
 
+app.get('/weather', (req, resp) => {
+  let dataWeather = require('./data/weather.json');
+  let allWeather = [];
+
+  dataWeather.forEach(dayData => {
+    let weather = new Weather(dayData);
+    allWeather.push(weather);
+
+  });
+  resp.status(200).json(allWeather);
+
+});
+
+function Weather(info){
+  this.forcast = info.weather.description;
+  this.datetime = info.datetime;
+}
+
+
 app.use('*', (req,resp) => {
   resp.status(404).send('Could Not Find!');
 });
@@ -36,12 +55,3 @@ app.use((error, req, resp, next) => {
 });
 
 app.listen( PORT, () => console.log('Working?', PORT));
-
-
-
-
-
-
-
-
-
